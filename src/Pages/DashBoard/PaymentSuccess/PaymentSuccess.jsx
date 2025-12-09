2f7deab62c8b831a7a609eb85b0ae716ec08c37f
Ahnaf Abid <ahnafabid600@gmail.com>
2025-12-09 22:26:14 +0600

    fix error

--- src/Pages/DashBoard/PaymentSuccess/PaymentSuccess.jsx
+++ src/Pages/DashBoard/PaymentSuccess/PaymentSuccess.jsx
@@ -3,32 +3,32 @@ import { useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
-    const [searchParams] =useSearchParams();
+    const [searchParams] = useSearchParams();
     const [paymentInfo, setPaymentInfo] = useState({});
-    const sessionId = searchParams.get('session_id')
-    const axiosSecure = useAxiosSecure()
+    const sessionId = searchParams.get('session_id');
+    const axiosSecure = useAxiosSecure();
 
-    console.log(sessionId)
+    console.log(sessionId);
 
-    useEffect(()=>{
-        if(sessionId){
-            axiosSecure.patch(`payment-success?session_id=${sessionId}`)
-            .then(res=> {
-                console.log(res.data)
-                setPaymentInfo({
-                    transactionId: res.sata.transactionId,
-                    trackingId: res.data.trackingId
+    useEffect(() => {
+        if (sessionId) {
+            axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
+                .then(res => {
+                    console.log(res.data)
+                    setPaymentInfo({
+                        transactionId: res.data.transactionId,
+                        trackingId : res.data.trackingId
+                    })
                 })
-            })
         }
-    },[sessionId, axiosSecure])
 
+    }, [sessionId, axiosSecure])
 
     return (
         <div>
-            <h2 className="text-4xl">Payment Successful</h2>
-            <p>Your TransactionId:{paymentInfo.transactionId}</p>
-            <p>Your Tracking Id:{paymentInfo.res.data.trackingId} </p>
+            <h2 className="text-4xl">Payment successful</h2>
+            <p>Your TransactionId: {paymentInfo.transactionId}</p>
+            <p>Your Parcel Tracking id: {paymentInfo.trackingId}</p>
         </div>
     );
 };
